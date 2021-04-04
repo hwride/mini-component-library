baa874d4bd66c7a712bdbcf76ee55b0d7fdb206d
Hugo Wride <hugowride@gmail.com>
2021-04-04 14:58:06 +0100

    Add basic small medium and large progress bar.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,59 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const MIN = 0;
+const MAX = 100;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Wrapper value={value}>
+    <Bar value={value} size={size} />
+  </Wrapper>;
 };
+
+const Wrapper = styled.div.attrs(props => ({
+  role: 'progressbar',
+  'aria-valuemin': MIN,
+  'aria-valuenow': props.value,
+  'aria-valuemax': MAX,
+}))`
+  background: ${COLORS.transparentGray15};
+  border-radius: ${({size}) => {
+    if(size === 'large') {
+      return '8px';
+    } else {
+      return '4px';
+    }
+  }}
+`;
+
+const Bar = styled.div`
+  height: ${props => {
+    const {size} = props;
+    if(size === 'small') {
+      return '8px';
+    } else if(size === 'large') {
+      return '16px';
+    } else {
+      return '12px';
+    }
+  }};
+  width: ${props => getPercent(props.value, MAX)}%;
+  background: ${COLORS.primary};
+  border-radius: ${props => {
+    const {size} = props;
+    if(size === 'small') {
+      return '4px 0px 0px 4px';
+    } else if(size === 'large') {
+      return '8px 0px 0px 8px';
+    } else {
+      return '4px 0px 0px 4px';
+    }
+  }};
+`
+
+function getPercent(a, b) {
+  debugger
+  return ((a/b) * 100);
+}
 
 export default ProgressBar;
